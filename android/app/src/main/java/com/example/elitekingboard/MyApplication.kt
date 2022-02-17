62fe70c35be06fffee5c9754948e356de1c04fc8
YangWoo Yu <yuyw0712@naver.com>
2022-02-17 16:23:41 +0900

    feat(#16): 기초 레이아웃 구현 1. BaseLayout 코드 구현 2. Navigation Component 적용 3. Koin 사용을 위한 Application 등록 4. data class resource util 구현

---
+++ android/app/src/main/java/com/example/elitekingboard/MyApplication.kt
@@ -0,0 +1,22 @@
+package com.example.elitekingboard
+
+import android.app.Application
+import androidx.databinding.ktx.BuildConfig
+import com.example.elitekingboard.data.di.ViewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@MyApplication)
+            modules(listOf(ViewModelModule))
+        }
+
+    }
+}
