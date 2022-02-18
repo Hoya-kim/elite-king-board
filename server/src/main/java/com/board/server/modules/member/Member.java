53ce46d58094db6b0b0ff44412518265aa6ada67
kimtaejun97 <kim_taejun@naver.com>
2022-02-18 14:33:48 +0900

    feat(#10): Member domain 구현

---
+++ server/src/main/java/com/board/server/modules/member/Member.java
@@ -0,0 +1,43 @@
+package com.board.server.modules.member;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Getter
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Getter @Setter private String nickname;
+    
+    @Column(nullable = false)
+    @Setter private String password;
+    
+    @Column(nullable = false, unique = true)
+    @Getter @Setter private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Getter @Setter private Role role;
+
+    @Column(nullable = false)
+    @Getter private LocalDateTime joinedAt;
+
+    public Member(String nickname, String password, String email, Role role) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
