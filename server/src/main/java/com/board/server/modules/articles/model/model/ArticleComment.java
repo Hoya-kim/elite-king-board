f76898f623f02f788648f127e1aea653306a9c3f
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-23 05:31:28 +0900

    feat(#25): ArticleComment 엔티티 생성

---
+++ server/src/main/java/com/board/server/modules/articles/model/model/ArticleComment.java
@@ -0,0 +1,33 @@
+package com.board.server.modules.articles.model.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "article_comment")
+public class ArticleComment {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "article_comment_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
