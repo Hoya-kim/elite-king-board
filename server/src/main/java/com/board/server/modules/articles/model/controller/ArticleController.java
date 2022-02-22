63647f371b52c236ec15067a3a214acd21d3896e
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-23 05:27:45 +0900

    feat(#25): Controller, Service 생성

---
+++ server/src/main/java/com/board/server/modules/articles/model/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.board.server.modules.articles.model.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/articles")
+@RestController
+public class ArticleController {
+
+}
---
+++ server/src/main/java/com/board/server/modules/articles/model/service/ArticleService.java
@@ -0,0 +1,8 @@
+package com.board.server.modules.articles.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+}
