9954bc84d11fb6344ae6ae2e7c5c0335f58e0b64
kimtaejun97 <kim_taejun@naver.com>
2022-02-23 03:36:56 +0900

    refactor(#10): setter -> 생성자 이용하도록 변경.

--- server/src/main/java/com/board/server/modules/member/mapper/AccountMapper.java
+++ server/src/main/java/com/board/server/modules/member/mapper/AccountMapper.java
@@ -6,12 +6,6 @@ import com.board.server.modules.member.dto.SignUpResponseDto;
 public class AccountMapper {
 
     public SignUpResponseDto toResponseDto(Account account) {
-        SignUpResponseDto responseDto = new SignUpResponseDto();
-        responseDto.setNickname(account.getNickname());
-        responseDto.setEmail(account.getEmail());
-        responseDto.setRole(account.getRole());
-        responseDto.setJoinedAt(account.getJoinedAt());
-
-        return responseDto;
+        return new SignUpResponseDto(account);
     }
 }
