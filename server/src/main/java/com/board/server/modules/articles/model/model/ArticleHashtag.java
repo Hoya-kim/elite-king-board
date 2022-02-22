fee254aa780159369601c83e30f8eeb2a0156a2e
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-23 05:31:45 +0900

    feat(#25): ArticleHashtag 엔티티 생성

---
+++ server/src/main/java/com/board/server/modules/articles/model/model/ArticleHashtag.java
@@ -0,0 +1,31 @@
+package com.board.server.modules.articles.model.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "article_hashtag")
+public class ArticleHashtag {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "article_hashtag_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "hashtag_id")
+    private HashTag hashTag;
+}
