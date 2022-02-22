7e91b3579157220aff61182cd7a6456b73b11fb8
kimtaejun97 <kim_taejun@naver.com>
2022-02-23 03:28:36 +0900

    feat(#10): 회원 가입 처리 로직 구현
    - 요청 정보 캐시에 임시저장
    - 인증 메일 전송.

--- server/src/main/java/com/board/server/modules/member/AccountController.java
+++ server/src/main/java/com/board/server/modules/member/AccountController.java
@@ -1,29 +1,52 @@
 package com.board.server.modules.member;
 
 import com.board.server.modules.member.dto.SignUpRequestDto;
-import com.board.server.modules.member.dto.SignUpResponseDto;
-import com.board.server.modules.member.mapper.AccountMapper;
-import java.net.URI;
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AccountController {
 
     private final AccountService accountService;
-    private final AccountMapper accountMapper;
 
-    @GetMapping("/members/sign-up")
-    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signupRequestDto) {
-        Account newAccount = accountService.signUp(signupRequestDto);
-        SignUpResponseDto responseDto = accountMapper.toResponseDto(newAccount);
+    @PostMapping("/members/sign-up")
+    public ResponseEntity signUp(@RequestBody @Valid SignUpRequestDto signupRequestDto)
+            throws MessagingException {
+        accountService.signUp(signupRequestDto);
 
-        return ResponseEntity
-                .created(URI.create("/members/" + newAccount.getId()))
-                .body(responseDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler
+    public String MessagingExceptionHandler(MessagingException exception, HttpServletRequest request) {
+        log.info("{}의 {}요청 인증메일 전송 실패", getRemoteAddress(request), request.getRequestURI());
+        log.error("Server Error: ", exception.getMessage());
+
+        return "/error/5xx";
+    }
+
+    private String getRemoteAddress(HttpServletRequest request) {
+        String remoteAddr = request.getHeader("X-FORWARDED-FOR");
+
+        if (remoteAddr == null) {
+            return request.getHeader("Proxy-Client-IP");
+        }
+        if (remoteAddr == null) {
+            return request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (remoteAddr == null) {
+            return request.getRemoteAddr();
+        }
+        return remoteAddr;
     }
 }
--- server/src/main/java/com/board/server/modules/member/AccountService.java
+++ server/src/main/java/com/board/server/modules/member/AccountService.java
@@ -1,10 +1,18 @@
 package com.board.server.modules.member;
 
+import com.board.server.config.AppProperties;
+import com.board.server.infra.mail.EmailMessage;
+import com.board.server.infra.mail.EmailService;
 import com.board.server.modules.member.dto.SignUpRequestDto;
+import java.util.UUID;
+import javax.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
 
 
 @Transactional(readOnly = true)
@@ -12,13 +20,52 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AccountService {
 
-    private final AccountRepository accountRepository;
-    private final PasswordEncoder passwordEncoder;
+    private static final String AUTHENTICATION_LINK_FORMAT = "%s/members/authentication-mail/?email=%s&token=%s";
+    private static final String ACCOUNT_CACHE_NAME = "accountTemp";
+    private static final String AUTHENTICATION_EMAIL_VIEW = "mail/authentication";
+    private static final String AUTHENTICATION_EMAIL_SUBJECT = "Elite King Board 인증 메일 입니다.";
 
-    @Transactional
-    public Account signUp(SignUpRequestDto signUpRequestDto) {
-        signUpRequestDto.setPassword(encode(signUpRequestDto.getPassword()));
-        return accountRepository.save(signUpRequestDto.toEntity());
+    private final PasswordEncoder passwordEncoder;
+    private final CacheManager cacheManager;
+    private final EmailService emailService;
+    private final ITemplateEngine templateEngine;
+    private final AppProperties appProperties;
+
+    public void signUp(SignUpRequestDto requestDto) throws MessagingException {
+        EmailMessage emailMessage = sendAuthenticationEmail(requestDto); //TODO 이름 변경.
+        emailService.send(emailMessage);
+
+        saveRequestToCache(requestDto);
+    }
+
+    private void saveRequestToCache(SignUpRequestDto requestDto) {
+        requestDto.setPassword(encode(requestDto.getPassword()));
+        cacheManager.getCache(ACCOUNT_CACHE_NAME).put(requestDto.getEmail(), requestDto);
+    }
+
+    private EmailMessage sendAuthenticationEmail(SignUpRequestDto requestDto) throws MessagingException {
+        requestDto.setAuthenticationToken(UUID.randomUUID().toString());
+
+        Context context = createContext(requestDto);
+        String view = templateEngine.process(AUTHENTICATION_EMAIL_VIEW, context);
+
+        EmailMessage message = EmailMessage.builder()
+                .to(requestDto.getEmail())
+                .subject(AUTHENTICATION_EMAIL_SUBJECT)
+                .text(view)
+                .build();
+
+        return message;
+    }
+
+    private Context createContext(SignUpRequestDto requestDto) {
+        Context context = new Context();
+        context.setVariable("nickname", requestDto.getNickname());
+        context.setVariable("link",
+                String.format(AUTHENTICATION_LINK_FORMAT, appProperties.getHost(), requestDto.getEmail(),
+                        requestDto.getAuthenticationToken()));
+
+        return context;
     }
 
     private String encode(String password) {
