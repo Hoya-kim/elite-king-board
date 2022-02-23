a088005f1a2ac362ae386f506a2fdcece5a452a4
kimtaejun97 <kim_taejun@naver.com>
2022-02-23 22:44:31 +0900

    feat(#27): existsByEmail 기능 추가

--- server/src/main/java/com/board/server/modules/member/AccountRepository.java
+++ server/src/main/java/com/board/server/modules/member/AccountRepository.java
@@ -3,4 +3,6 @@ package com.board.server.modules.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmail(String email);
 }
