b33b9be00089c72f687ebb63a469f1db294d927c
kimtaejun97 <kim_taejun@naver.com>
2022-02-24 21:03:22 +0900

    feat(#33): JWT를 사용하기 위한 config 설정
    - 세션 끄기, cors모두 허용

--- server/src/main/java/board/api/config/SecurityConfig.java
+++ server/src/main/java/board/api/config/SecurityConfig.java
@@ -1,22 +1,37 @@
 package board.api.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CorsFilter corsFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.
+                csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter);
 
-        http.authorizeRequests()
+        http.
+                authorizeRequests()
             .mvcMatchers("/", "/accounts/sign-up", "/login", "/accounts/authentication-mail")
             .permitAll()
             .anyRequest().authenticated();
+
+        http.
+                formLogin().disable()
+                .httpBasic().disable();
     }
 
     @Override
