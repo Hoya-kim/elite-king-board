2944ad2019203290fdbd6151f6552493c31df75c
kimtaejun97 <kim_taejun@naver.com>
2022-02-24 21:16:43 +0900

    feat(#33): 이메일로 DB에서 계정 찾기

--- server/src/main/java/board/api/modules/account/AccountRepository.java
+++ server/src/main/java/board/api/modules/account/AccountRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByEmail(String email);
+
+    Account findByEmail(String email);
 }
