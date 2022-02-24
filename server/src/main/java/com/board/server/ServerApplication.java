6ef4f6d2152483b94a1b7fee4d8bbb0f26e8d7e2
kimtaejun97 <kim_taejun@naver.com>
2022-02-24 11:56:34 +0900

    feat(#27): 엔티티 생성 시점에 생성 시간이 자동으로 들어가도록 변경.
    - null 이 들어가면 DB의 default 가 먹히지 않아 애플리케이션에서도 설정.

--- server/src/main/java/com/board/server/ServerApplication.java
+++ server/src/main/java/com/board/server/ServerApplication.java
@@ -3,7 +3,9 @@ package com.board.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class ServerApplication {
--- server/src/main/java/com/board/server/modules/member/Account.java
+++ server/src/main/java/com/board/server/modules/member/Account.java
@@ -3,6 +3,7 @@ package com.board.server.modules.member;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -10,9 +11,10 @@ import javax.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id", "email"})
 @Entity
@@ -38,9 +40,9 @@ public class Account {
     @Getter
     private Role role;
 
-    @DateTimeFormat(iso = ISO.DATE_TIME)
-//    @Column(nullable = false)
     @Getter
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime joinedAt;
 
     public Account(String nickname, String password, String email, Role role) {
