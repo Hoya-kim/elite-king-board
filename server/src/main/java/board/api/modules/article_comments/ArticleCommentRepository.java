c2ac3c63d3fc1cec125b4aa413844f2bab5469b5
gal2o <hunmin8788@gmail.com>
2022-02-24 23:15:28 +0900

    feat(#9): ArticleCommentRepository 생성

---
+++ server/src/main/java/board/api/modules/article_comments/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package board.api.modules.article_comments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+}
