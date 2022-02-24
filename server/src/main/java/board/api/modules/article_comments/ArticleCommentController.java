38f185445ae036d926b5fcc6ee633d5e2940393b
gal2o <hunmin8788@gmail.com>
2022-02-25 00:04:22 +0900

    feat(#9): 게시글 댓글 등록 api 생성

--- server/src/main/java/board/api/modules/article_comments/ArticleCommentController.java
+++ server/src/main/java/board/api/modules/article_comments/ArticleCommentController.java
@@ -1,10 +1,28 @@
 package board.api.modules.article_comments;
 
+import board.api.modules.article_comments.dto.ArticleCommentRequestDto;
+import board.api.modules.article_comments.dto.ArticleCommentResponseDto;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+    @PostMapping("/articles/{articleId}/article-comments")
+    public ResponseEntity<ArticleCommentResponseDto> addComment(
+        @PathVariable Long articleId,
+        @RequestBody @Valid ArticleCommentRequestDto.Post articleCommentRequestDto) {
+
+        return ResponseEntity.ok(
+            articleCommentService.addComment(articleId, articleCommentRequestDto));
+    }
 
 }
--- server/src/main/java/board/api/modules/article_comments/ArticleCommentService.java
+++ server/src/main/java/board/api/modules/article_comments/ArticleCommentService.java
@@ -1,10 +1,35 @@
 package board.api.modules.article_comments;
 
+import board.api.modules.article_comments.dto.ArticleCommentRequestDto;
+import board.api.modules.article_comments.dto.ArticleCommentResponseDto;
+import board.api.modules.articles.model.Article;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleRepository articleRepository;
+    private final ModelMapper modelMapper;
+
+    private final String INVALID_ARTICLE_MESSAGE = "존재하지 않는 게시글입니다.";
+
+    @Transactional
+    public ArticleCommentResponseDto addComment(
+        Long articleId,
+        ArticleCommentRequestDto.Post articleCommentRequestDto
+    ) {
+        Article article = articleRepository.findById(articleId)
+            .orElseThrow(() -> new IllegalArgumentException(INVALID_ARTICLE_MESSAGE));
+
+        ArticleComment articleComment = articleCommentRepository.save(
+            articleCommentRequestDto.toEntity(article));
+
+        return modelMapper.map(articleComment, ArticleCommentResponseDto.class);
+    }
 
 }
