519338f3d73a825ad3e5c4ade5d891b34f4a8b8c
kimtaejun97 <kim_taejun@naver.com>
2022-02-25 00:58:56 +0900

    test(#33): 로그인 - Jwt 토큰 발급 테스트

--- server/src/test/java/board/api/modules/account/AccountControllerIntegrationTest.java
+++ server/src/test/java/board/api/modules/account/AccountControllerIntegrationTest.java
@@ -1,8 +1,12 @@
 package board.api.modules.account;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -23,8 +29,12 @@ public class AccountControllerIntegrationTest {
     private AccountRepository accountRepository;
     @Autowired
     private CacheManager cacheManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private String requestContent;
+
 
     @BeforeEach
     void sut() {
@@ -39,9 +49,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(requestContent)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(requestContent)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - Repository 이미 존재하는 이메일")
@@ -49,13 +59,13 @@ public class AccountControllerIntegrationTest {
     void GivenRequest_WhenExistsEmail_ThenThrowException() throws Exception {
         // Given
         accountRepository.save(
-            new Account("kim", "12341234", "taejun0509@11stcorp.com", Role.USER));
+                new Account("kim", "12341234", "taejun0509@11stcorp.com", Role.USER));
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(requestContent)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(requestContent)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 닉네임에 유효하지 않은 특수문자가 존재")
@@ -65,9 +75,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 너무 짧은 이름")
@@ -77,9 +87,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 너무 긴 이름")
@@ -89,9 +99,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 11번가 이메일이 아님")
@@ -101,9 +111,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 너무 짧은 비밀번호")
@@ -113,9 +123,9 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @DisplayName("회원 가입을 요청을 받는다. - 너무 긴 비밀번호")
@@ -125,8 +135,29 @@ public class AccountControllerIntegrationTest {
 
         // When & Then
         mvc.perform(post("/accounts/sign-up")
-                .content(request)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is4xxClientError());
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @DisplayName("로그인을 시도하여 JWT 토큰을 발급 받는다.")
+    @Test
+    void givenAccount_whenLogin_thenJwtToken() throws Exception {
+        // Given
+        Map<String, String> userInfo = new HashMap<>() {
+            {
+                put("email", "taejun0509@11stcorp.com");
+                put("password", "12341234");
+            }
+        };
+        String request = objectMapper.writeValueAsString(userInfo);
+        accountRepository.save(new Account("kimtaejun", passwordEncoder.encode("12341234"), "taejun0509@11stcorp.com", Role.USER));
+
+        // When & Then
+        mvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isOk())
+                .andExpect(header().exists(HttpHeaders.AUTHORIZATION));
     }
 }
