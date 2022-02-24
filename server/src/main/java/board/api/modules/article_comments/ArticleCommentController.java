a06c0cae4f48a256a3b3fd1f66f97615f8ea6405
gal2o <hunmin8788@gmail.com>
2022-02-24 23:27:27 +0900

    feat(#9): ArticleCommentController 생성

---
+++ server/src/main/java/board/api/modules/article_comments/ArticleCommentController.java
@@ -0,0 +1,10 @@
+package board.api.modules.article_comments;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ArticleCommentController {
+
+}
