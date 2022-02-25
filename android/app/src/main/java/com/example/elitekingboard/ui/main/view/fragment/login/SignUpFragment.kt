57555be5b3f6163a4af8669d48d1edf5a9fbf745
YangWoo Yu <yuyw0712@naver.com>
2022-02-25 13:33:35 +0900

    feat(#29): 회원가입 인증 요청 api 연동

--- android/app/src/main/java/com/example/elitekingboard/ui/main/view/fragment/login/SignUpFragment.kt
+++ android/app/src/main/java/com/example/elitekingboard/ui/main/view/fragment/login/SignUpFragment.kt
@@ -1,8 +1,44 @@
 package com.example.elitekingboard.ui.main.view.fragment.login
 
+import androidx.lifecycle.Observer
 import com.example.elitekingboard.R
 import com.example.elitekingboard.databinding.FragmentSignupBinding
 import com.example.elitekingboard.ui.base.BaseFragment
+import com.example.elitekingboard.ui.main.view.dialog.ProgressDialog
+import com.example.elitekingboard.ui.main.view.viewmodel.LoginViewModel
+import com.example.elitekingboard.util.Resource
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SignUpFragment : BaseFragment<FragmentSignupBinding>(R.layout.fragment_signup) {
+    private val loginViewModel: LoginViewModel by sharedViewModel()
+    private val progressDialog: ProgressDialog by lazy { ProgressDialog(requireContext()) }
+
+    override fun init() {
+        super.init()
+        requestSignUpData()
+    }
+
+    private fun requestSignUpData() {
+        binding.btnSignUp.setOnClickListener {
+            loginViewModel.signUpResponse.observe(viewLifecycleOwner, Observer { resource ->
+                when(resource.status) {
+                    Resource.Status.SUCCESS -> {
+                        when(resource.data?.code()) {
+                            200 -> {
+//                                resource.data.headers()
+                            }
+                        }
+                    }
+                    Resource.Status.LOADING -> {
+                        progressDialog.show()
+                    }
+                    Resource.Status.ERROR -> {
+                        progressDialog.dismiss()
+                        toast(requireContext(), "${resource.message}")
+                    }
+                }
+            })
+        }
+    }
+
 }
